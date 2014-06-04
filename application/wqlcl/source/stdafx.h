956dc8d1551ecd2e4f6d0e953e1e0cabe54f64b3
jcyuan78 <jcyuan78@49ecab2b-0442-cfd4-625f-8bf831dead6f>
2014-06-04 01:01:11 +0000

    initial add wqlcl

---
+++ application/wqlcl/source/stdafx.h
@@ -0,0 +1,26 @@
+﻿// stdafx.h : 标准系统包含文件的包含文件，
+// 或是经常使用但不常更改的
+// 特定于项目的包含文件
+//
+
+#pragma once
+
+// 以下宏定义要求的最低平台。要求的最低平台
+// 是具有运行应用程序所需功能的 Windows、Internet Explorer 等产品的
+// 最早版本。通过在指定版本及更低版本的平台上启用所有可用的功能，宏可以
+// 正常工作。
+
+// 如果必须要针对低于以下指定版本的平台，请修改下列定义。
+// 有关不同平台对应值的最新信息，请参考 MSDN。
+#ifndef _WIN32_WINNT            // 指定要求的最低平台是 Windows Vista。
+#define _WIN32_WINNT 0x0600     // 将此值更改为相应的值，以适用于 Windows 的其他版本。
+#endif
+
+#define LOGGER_LEVEL LOGGER_LEVEL_DEBUGINFO
+
+#include <stdio.h>
+#include <tchar.h>
+
+
+
+// TODO: 在此处引用程序需要的其他头文件
