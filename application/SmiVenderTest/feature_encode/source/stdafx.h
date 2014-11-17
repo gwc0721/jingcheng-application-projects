e556ca28e0f50f4035229dace91cf09852e80802
jcyuan78 <jcyuan78@49ecab2b-0442-cfd4-625f-8bf831dead6f>
2014-11-17 07:05:36 +0000

    initial add feature encode

---
+++ application/SmiVenderTest/feature_encode/source/stdafx.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifndef _WIN32_WINNT		//                
+#define _WIN32_WINNT 0x0501	//
+#endif	
+
+#define LOGGER_LEVEL LOGGER_LEVEL_DEBUGINFO
+#define LOG_OUT_CLASS_SIZE
+
+#include <stdio.h>
+#include <tchar.h>
