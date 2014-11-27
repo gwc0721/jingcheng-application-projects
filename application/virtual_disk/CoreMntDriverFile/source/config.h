d8924d62c0341dd75fddac6e8262503f8d003038
jcyuan78 <jcyuan78@49ecab2b-0442-cfd4-625f-8bf831dead6f>
2014-11-27 01:14:05 +0000

    update for configuration test

--- application/virtual_disk/CoreMntDriverFile/source/config.h
+++ application/virtual_disk/CoreMntDriverFile/source/config.h
@@ -1,16 +1,16 @@
 #pragma once
 
 // vendor command entry read 0x55AAŽù—v•Ô‰ñ“I“à—e
-#define FN_VENDOR_BIN	_T("data\\vendor.bin")
-#define FN_FID_BIN		_T("data\\fid.bin")
-#define FN_SFR_BIN		_T("data\\sfr.bin")
-#define FN_PAR_BIN		_T("data\\par.bin")
-#define FN_IDTAB_BIN	_T("data\\idtable.bin")
-#define FN_DEVINFO_BIN	_T("data\\device_info.bin")
-#define FN_ISP_BIN		_T("data\\isp.bin")
-#define FN_INFO_BIN		_T("data\\info.bin")
-#define FN_BOOTISP_BIN	_T("data\\bootisp.bin")
-#define FN_ORGBAD_BIN	_T("data\\org_bad.bin")
+#define FN_VENDOR_BIN	_T("vendor_signe.bin")
+#define FN_FID_BIN		_T("fid.bin")
+#define FN_SFR_BIN		_T("sfr.bin")
+#define FN_PAR_BIN		_T("par.bin")
+#define FN_IDTAB_BIN	_T("idtable.bin")
+#define FN_DEVINFO_BIN	_T("device_info.bin")
+#define FN_ISP_BIN		_T("isp.bin")
+#define FN_INFO_BIN		_T("info.bin")
+#define FN_BOOTISP_BIN	_T("bootisp.bin")
+#define FN_ORGBAD_BIN	_T("org_bad.bin")
 
 #define FN_MPISP_LOG	_T("log\\mpisp.bin")
 #define FN_PRETEST_LOG	_T("log\\pretest.bin")
@@ -19,3 +19,12 @@
 #define MAX_ISP_SEC		1024
 #define BOOTISP_SIZE	2
 
+#define MAX_STRING_LEN	256
+
+#define SEG_SYSTEM_INFO		_T("system_info")
+#define FIELD_DATA_FOLDER	_T("data_folder")
+#define FIELD_MAX_ISP_LEN	_T("max_isp_len")	
+
+#define SEG_STORAGE			_T("storage")
+#define FIELD_STORAGE_FILE	_T("storage_file")
+#define FIELD_TOTAL_SEC		_T("total_sectors")
