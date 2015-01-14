0520331f1832aeae64075e6e8922fc3fb498559f
jcyuan78 <jcyuan78@49ecab2b-0442-cfd4-625f-8bf831dead6f>
2015-01-14 02:50:47 +0000

    1, using local_config.h to configure jcapp single tone support 2, update single tone support 3, add version, help message ant etc, to jcappbase

--- jcvos/jcapp/source/application.cpp
+++ jcvos/jcapp/source/application.cpp
@@ -1,6 +1,9 @@
 #include "stdafx.h"
 
 #include "../include/application.h"
+#include <iostream>
+#include <conio.h>
+
 
 using namespace jcapp;
 
@@ -11,6 +14,7 @@ const GUID jcapp::JCAPP_GUID =
 
 AppArguSupport::AppArguSupport(DWORD prop)
 	: m_src_file(NULL), m_dst_file(NULL), m_prop(prop)
+	//, m_show_help(false)
 {
 	
 }
@@ -39,8 +43,9 @@ bool AppArguSupport::CmdLineParse(BYTE * base)
 	bool br = m_cmd_parser.Parse(GetCommandLine(), base);
 	m_cmd_parser.m_remain.GetValT(ARGU_SRC_FN, m_src_fn);
 	m_cmd_parser.m_remain.GetValT(ARGU_DST_FN, m_dst_fn);
+	if ( m_cmd_parser.m_remain.Exist(ARGU_HELP) ) throw std::exception("show help\n");
 
-	return true;
+	return br;
 }
 
 FILE * AppArguSupport::OpenInputFile(void)
@@ -72,47 +77,17 @@ void AppArguSupport::ArguCleanUp(void)
 	if ( (m_dst_file != stdout)	&& (m_dst_file != NULL) )	fclose(m_dst_file);
 }
 
+//void AppArguSupport::ShowHelpInfo(FILE *out);
 
 ///////////////////////////////////////////////////////////////////////////////
 // --
-/*
-bool CJCAppBase::CmdLineParse(BYTE *base)
+CJCAppBase * CJCAppBase::m_instance = NULL;
+
+CJCAppBase::CJCAppBase(void)
 {
-	if (prop & ARGU_SUPPORT)
-	{
-		if (m_prop & ARGU_SUPPORT_INFILE)
-		{
-			m_cmd_parser.AddParamDefine( new jcparam::CArguDescBase(ARGU_SRC_FN, 
-				ABBR_SRC_FN, jcparam::VT_STRING, _T("input file, default stdin")) );
-		}
-
-		if (m_prop & ARGU_SUPPORT_OUTFILE)
-		{
-			m_cmd_parser.AddParamDefine( new jcparam::CArguDescBase(ARGU_DST_FN, 
-				ABBR_DST_FN, jcparam::VT_STRING, _T("output file, default stdout")) );
-		}
-
-		if (m_prop & ARGU_SUPPORT_HELP)
-		{
-			m_cmd_parser.AddParamDefine( new jcparam::CArguDescBase(ARGU_HELP,
-				ABBR_HELP, jcparam::VT_BOOL, _T("show this message")) );
-		}
-
-		bool br = m_cmd_parser.Parse(GetCommandLine(), base);
-		m_cmd_parser.m_remain.GetValT(ARGU_SRC_FN, m_src_fn);
-		m_cmd_parser.m_remain.GetValT(ARGU_DST_FN, m_dst_fn);
-
-		return true;
-	}
+	memset(m_ver, 0, sizeof(UINT) * 4);
+	LoadApplicationInfo();
 }
-
-void CJCAppBase::CleanUp(void)
-{
-	if ( (m_src_file != stdin) && (m_src_file != NULL) )	fclose(m_src_file);
-	if ( (m_dst_file != stdout)	&& (m_dst_file != NULL) )	fclose(m_dst_file);
-}
-
-*/
 
 void CJCAppBase::GetAppPath(CJCStringT &path)
 {
@@ -123,15 +98,108 @@ void CJCAppBase::GetAppPath(CJCStringT &path)
 	path = str;
 }
 
+void CJCAppBase::ShowVersionInfo(FILE * out)
+{
+	_ftprintf_s(out, _T("\t ver %d.%d.%d.%d\n"), m_ver[0], m_ver[1], m_ver[2], m_ver[3]);
+}
+
+void CJCAppBase::ShowAppInfo(FILE * out)
+{
+	_ftprintf_s(out, AppDescription());
+	ShowVersionInfo(out);
+}
+
 CJCAppBase * CJCAppBase::GetApp(void)
 {
-	static CJCAppBase * instance = NULL;
-	if (instance == NULL)	
+	//static CJCAppBase * instance = NULL;
+	if (m_instance == NULL)	
 	{
+#if APP_GLOBAL_SINGLE_TONE > 0
+
 		CSingleToneEntry * entry = CSingleToneEntry::Instance();
 		CSingleToneBase * ptr = NULL;
 		entry->QueryStInstance(JCAPP_GUID, ptr);
 		instance = dynamic_cast<CJCAppBase*>(ptr);
+#endif
 	}
-	return instance;
+	JCASSERT(m_instance);
+	return m_instance;
+}
+
+bool CJCAppBase::LoadApplicationInfo(void)
+{
+#ifdef WIN32
+	//TCHAR app_path[MAX_PATH];
+	stdext::auto_array<TCHAR> str_temp(MAX_PATH);
+
+	GetModuleFileName(NULL, str_temp, MAX_PATH);
+	UINT ver_info_size = GetFileVersionInfoSize(str_temp, 0);
+
+	stdext::auto_array<BYTE> ver_buf(ver_info_size);
+
+	BOOL br = GetFileVersionInfo(str_temp, 0, ver_info_size, ver_buf);
+	if ( 0 == br )	return false;
+	
+	// Get language
+    struct VersionLanguage
+    {
+        WORD m_wLanguage;
+        WORD m_wCcodePage;
+    };
+	VersionLanguage * p_ver_lang = NULL;
+	UINT length = 0;
+	
+	br = VerQueryValue(ver_buf, _T("\\VarFileInfo\\Translation"), reinterpret_cast<LPVOID *>(&p_ver_lang), &length);
+	if ( 0==br) return false;
+    
+	// load application version
+	_stprintf_s(str_temp, MAX_PATH, _T("\\StringFileInfo\\%04x%04x\\FileVersion"),
+		p_ver_lang->m_wLanguage, p_ver_lang->m_wCcodePage);
+
+	LPVOID p_prod_ver = NULL;
+	length = 0;
+	br = VerQueryValue(ver_buf, str_temp, &p_prod_ver, &length);
+	if ( 0==br) return false;
+	
+	//LOG_RELEASE(_T("File Ver. %s"), reinterpret_cast<TCHAR *>(p_prod_ver) );
+
+	_stscanf_s(reinterpret_cast<TCHAR *>(p_prod_ver), _T("%d, %d, %d, %d"), 
+		m_ver+0, m_ver+1, m_ver+2, m_ver+3);
+#endif
+	return true;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//-- main
+int jcapp::local_main(int argc, _TCHAR* argv[])
+{
+	int ret_code = 0;
+	CJCAppBase * app = CJCAppBase::GetApp();
+	app->ShowAppInfo(stdout);
+	bool show_help = false;
+
+	JCASSERT(app);
+	try
+	{
+		app->Initialize();
+		ret_code = app->Run();
+	}
+	catch (stdext::CJCException & err)
+	{
+		stdext::jc_fprintf(stderr, _T("error: %s\n"), err.WhatT() );
+		ret_code = err.GetErrorID();
+		if ( ret_code == stdext::CJCException::ERR_ARGUMENT) show_help = true;
+	}
+    catch(const std::exception & ex)
+    {
+        std::cout << ex.what();
+		ret_code = -1;
+		show_help = true;
+    }
+	if (show_help ) app->ShowHelpInfo(stdout);
+	app->CleanUp();
+
+	//stdext::jc_printf(_T("Press any key to continue..."));
+	//getc(stdin);
+	return ret_code;
 }
