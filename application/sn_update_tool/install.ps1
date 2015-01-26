4f56921e051a60468868ea50d0cc6eb5809291b9
jcyuan78 <jcyuan78@49ecab2b-0442-cfd4-625f-8bf831dead6f>
2015-01-26 11:08:10 +0000

    1, update for using package tool. 2, add install tool.

---
+++ application/sn_update_tool/install.ps1
@@ -0,0 +1,18 @@
+﻿#参数：
+param($tar, $DBG)
+
+if ( test-path -path $tar )
+{
+	remove-item -force -recurse -path $tar
+}
+md -force $tar
+cp -recurse test_data\* $tar\.
+cp FerriSdk\SDK\* $tar\.
+if ($DBG -eq "DEBUG")
+{
+	cp DEBUG_STATIC_MFC\UpdateSnTool.exe $tar\.
+}
+else
+{
+	cp RELEASE_STATIC_MFC\UpdateSnTool.exe $tar\.
+}
