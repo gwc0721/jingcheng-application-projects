dd636e6ca674910e583d807b44bf3f03c79357b7
jcyuan78 <jcyuan78@49ecab2b-0442-cfd4-625f-8bf831dead6f>
2015-01-30 05:24:07 +0000

    update version info.

--- application/sn_update_tool/res/resource.h
+++ application/sn_update_tool/res/resource.h
@@ -1,6 +1,6 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by sm224testB.rc
+// Used by UpdateSnTool.rc
 //
 #define IDS_FINDNEWDEVICE               1
 #define IDS_HARDWAREINSTALL             2
